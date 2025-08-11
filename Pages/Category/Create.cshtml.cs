6c3d6c29c747a57f65677a4bfb1fd040859d8fce
huot <khouvtannhuot168@gmail.com>
2025-08-11 10:58:58 +0700

    init project

---
+++ Pages/Category/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace blog_m15.Pages.Category
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
