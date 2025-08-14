5a8eaee28909cf0d8763c9cda206ccb41e903f24
huot <khouvtannhuot168@gmail.com>
2025-08-14 11:16:07 +0700

    complete cateogry module

--- Pages/Category/Edit.cshtml.cs
+++ Pages/Category/Edit.cshtml.cs
@@ -1,12 +1,31 @@
+using blog_m15.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace blog_m15.Pages.Category
 {
-    public class EditModel : PageModel
+    public class EditModel(AppDBContext _db) : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public CategoryEntiry Category { get; set; }
+
+        public async Task OnGet(int id)
         {
+            Category = await _db.Categories.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var existCate = await _db.Categories.FindAsync(Category.Id);
+                existCate.Name = Category.Name;
+                await _db.SaveChangesAsync();
+                return RedirectToPage("/category/index");
+            }
+
+            return Page();
         }
     }
 }
--- Pages/Category/Index.cshtml.cs
+++ Pages/Category/Index.cshtml.cs
@@ -8,14 +8,26 @@ namespace blog_m15.Pages.Category
     public class IndexModel(AppDBContext _db) : PageModel
     {
         public IEnumerable<CategoryEntiry> Categories { get; set; }
+
+        [BindProperty]
+        public string SearchText { get; set; } = "";
         public async Task OnGet()
         {
             Categories = await _db.Categories.ToListAsync();
         }
 
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPost(int id)
         {
-            Categories = await _db.Categories.ToListAsync();
+            var cate = await _db.Categories.FindAsync(id);
+            _db.Categories.Remove(cate);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostSearch()
+        {
+            Categories = await _db.Categories.Where(c => c.Name.Contains("ca")).ToListAsync();
             return Page();
         }
     }
